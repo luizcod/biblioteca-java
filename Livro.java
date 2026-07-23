5eab76e09b3233919e95222394bd13eb621ca295
luizcod <dudu.nantes@gmail.com>
2026-07-23 19:09:43 -0300

    adiciona classe Livro

---
+++ Livro.java
@@ -0,0 +1,56 @@
+public class Lvro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private int ano;
+    private boolean disponibilidade;
+    
+    public Livro(int id, String titulo, String autor, int ano){
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.disponibilidade = true;
+    }
+  
+    public String getAutor(){
+        return autor;
+    }
+    public void setAutor(String autor){
+        this.autor = autor;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getTitulo(){
+        return titulo;
+    }
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+    public int getAno(){
+        return ano; 
+} public void setAno(int ano){
+        this.ano = ano;
+    }
+    public boolean getDisponibilidade(){
+        return disponibilidade;
+    }
+    public void setDisponibilidade(boolean disponibilidade){
+        this.disponibilidade = disponibilidade;
+    }
+@Override
+    public String toString() {
+        return "Livro{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", ano=" + ano +
+                ", disponibilidade=" + disponibilidade +
+                '}';
+    }
+
+}
